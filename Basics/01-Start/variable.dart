7b31bfe7a54be28df651a0fffa8486805552e5e4
Kartik Pant <kartikpant.kp69@gmail.com>
2026-03-29 16:55:14 +0530

    feat-variables

---
+++ Basics/01-Start/variable.dart
@@ -0,0 +1,20 @@
+// just same as other programming language 
+
+void main(){
+    bool child=true;
+    dynamic number=23.2;
+    // dynamic can be like auto in cpp can take any value 
+    // generally not recommended to use dynamic 
+}
+
+void main(){
+int n=12;
+int m=21;
+int temp;
+temp=n;
+n=m;
+m=temp;
+    print(n);
+    print(m);
+
+}
