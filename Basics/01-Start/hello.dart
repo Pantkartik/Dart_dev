a87846cd0f67db3ddbb690bf487483b2cff5b6c6
Kartik Pant <kartikpant.kp69@gmail.com>
2026-03-29 16:41:47 +0530

    feat-logic is mixture of js and python and cpp

--- Basics/01-Start/hello.dart
+++ Basics/01-Start/hello.dart
@@ -1,3 +1,8 @@
 void main(){
     print("Hello world");
+    // + - / * concepts in dart are the concept in javascript same 
+    // to concat the string we use the + operator and to 
+    print('3'+2); // this will give error , string + number not possible 
+    print('3'*2);  // this will give 33 as it multiply 3 two times
+    print('3'+'2');  // this will give 32
 }
