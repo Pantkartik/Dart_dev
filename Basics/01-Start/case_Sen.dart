082be864ac81d85c06367182ebed350fa47126ee
Kartik Pant <kartikpant.kp69@gmail.com>
2026-03-29 17:09:50 +0530

    feat-string interpolation and case sens

---
+++ Basics/01-Start/case_Sen.dart
@@ -0,0 +1,10 @@
+// dart is a case sensitive 
+
+void main(){
+    int og_val=69;
+    print(og_val);
+
+    og_val=90;
+    print(og_val*20);
+    print(og_val);
+}
---
+++ Basics/01-Start/string.dart
@@ -0,0 +1,9 @@
+// should use string interpolation when concat the string
+
+void main(){
+    String greeting='hello';
+    print(greeting+' Kartik ');  // this is not recomended
+
+    print('${greeting} Kartik !');
+
+}
