c5ed7df7e98416455f29ed9fd5e2c7999c0cecf3
Kartik Pant <kartikpant.kp69@gmail.com>
2026-03-29 18:07:45 +0530

    feat-while loop'

---
+++ Basics/Contol_Flow/loop.dart
@@ -0,0 +1,12 @@
+// same as cpp 
+
+void main(){
+    int i1=0;
+for(i1=0;i1<10;i1++){
+    print("For loop flow : ${i1}");
+}
+    while(i1<20){
+        print("While loop flow : ${i1}");
+        i1++;
+    }
+}
